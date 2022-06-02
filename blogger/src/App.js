a230c6f35103387de8b93a260c0573ec73ed2e9a
RJ2523 <rajneesh.sahay79@gmail.com>
2022-06-03 02:40:33 +0530

    added header

--- blogger/src/App.js
+++ blogger/src/App.js
@@ -2,13 +2,13 @@ import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import Auth from "./Components/Auth/Auth";
 import Blog from "./Components/Blog";
 import Home from "./Components/Home";
-
+import Headers from "./Components/Header";
+import "./Assets/Styles/main.css"
 
 function App() {
   return (
     <div className="App">
-      App
-
+      <Headers></Headers>
       <Router>
         <Routes>
           <Route path="/auth" exact
---
+++ blogger/src/Components/Header.js
@@ -0,0 +1,16 @@
+import "../Assets/Styles/header.css";
+
+// const headingStyle = {
+//     fontSize: "2rem",
+//   };
+  
+
+const Header = () => (
+    <div className="header">
+        <div className='heading'>Blogger</div>    
+    </div>
+);
+
+
+
+export default Header;
