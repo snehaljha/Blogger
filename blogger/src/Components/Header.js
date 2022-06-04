2fb2aaf2b12eaef6cebc4e7ff7ebd151d8a385d8
snehaljha <snehaljha08@gmail.com>
2022-06-04 22:01:41 +0530

    header and logout alignment fix

--- blogger/src/Components/Header.js
+++ blogger/src/Components/Header.js
@@ -2,13 +2,23 @@ import "../Assets/Styles/header.css";
 
 
 const Header = (props) => {
-  // const display = {
-  //   color : 'red',
-  // }; 
+  const displayStyle = {
+    display: props.user === undefined ? 'none' : 'block'
+  };
+
+  const justifyStyle = {
+    justifyContent: props.user === undefined ? 'center' : 'space-between'
+  };
+
+  const logOut = () => {
+    props.updateUser(undefined);
+  };
+
     return(
-      <div className="header">
-        <div className='heading'>Blogger</div>  
-        <a href="/auth" className = 'logout'>LogOut</a>
+      <div className="header" style={justifyStyle}>
+        <div className="spacer" style={displayStyle}></div>
+        <div className='heading'>Blogger</div>
+        <div className = 'logout' style={displayStyle} onClick={logOut}>LogOut</div>
 
     </div>
     )
